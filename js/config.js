95c11ad12d123bf861bb212e3387e56b4ea9ece8
JoyOmokore <damilolaomokore@gmail.com>
2025-03-27 17:13:36 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Yr0yauTuW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3auoc4rvr5stqdfj1vgcti14sk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4ggom0na8h7figk30rf8v3l57j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
